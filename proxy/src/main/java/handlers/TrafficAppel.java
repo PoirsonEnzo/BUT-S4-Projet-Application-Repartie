d5c22484b61271c380d9b97183db2fbbc0adcf1b
Nurd_Enzo <poirsonenzo@gmail.com>
2026-06-03 14:29:12 +0200

    Test handlers

--- proxy/src/main/java/handlers/TrafficAppel.java
+++ proxy/src/main/java/handlers/TrafficAppel.java
@@ -1,7 +1,22 @@
 package handlers;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-public class TrafficAppel implements HttpHandler {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+import static jdk.tools.jlink.internal.runtimelink.ResourceDiff.read;
+
+public class TrafficAppel implements HttpHandler {
+    public void handle(HttpExchange t) throws IOException {
+        InputStream is = t.getRequestBody();
+        read(is); // .. read the request body
+        String response = "This is the response";
+        t.sendResponseHeaders(200, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
 }
