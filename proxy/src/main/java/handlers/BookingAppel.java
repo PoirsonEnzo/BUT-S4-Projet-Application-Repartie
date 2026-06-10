66c78968545d9e02b6f0937f5a4f10f146e7d4e1
Nurd_Enzo <poirsonenzo@gmail.com>
2026-06-10 10:46:44 +0200

    Ajout Booking et Restaurant

--- proxy/src/main/java/handlers/BookingAppel.java
+++ proxy/src/main/java/handlers/BookingAppel.java
@@ -2,24 +2,76 @@ package handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+// tonpackage.ServiceRMI;
 
 public class BookingAppel implements HttpHandler {
-    public void handle(HttpExchange t) throws IOException {
-        InputStream is = t.getRequestBody();
-        read(is); // .. read the request body
-        String response = "This is the response";
-        t.sendResponseHeaders(200, response.length());
-        OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "POST, OPTIONS");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+
+        if ("OPTIONS".equalsIgnoreCase(exchange.getRequestMethod())) {
+            exchange.sendResponseHeaders(204, -1);
+            return;
+        }
+
+        if ("POST".equalsIgnoreCase(exchange.getRequestMethod())) {
+            try {
+                InputStream is = exchange.getRequestBody();
+                String requestBody = new String(is.readAllBytes(), "UTF-8");
+
+                int idRestau = Integer.parseInt(extractValeur(requestBody, "idRestau"));
+                int nbrPersonnes = Integer.parseInt(extractValeur(requestBody, "nbrPersonnes"));
+                String date = extractValeur(requestBody, "date");
+                String periode = extractValeur(requestBody, "periode");
+                String prenom = extractValeur(requestBody, "prenom");
+                String nom = extractValeur(requestBody, "nom");
+                String telephone = extractValeur(requestBody, "telephone");
+
+                Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+                ServiceRMI service = (ServiceRMI) registry.lookup("NomDuService");
+
+                String jsonResponse = service.reserverTable(idRestau, date, periode, nbrPersonnes, prenom, nom, telephone);
+
+                exchange.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
+                exchange.sendResponseHeaders(200, jsonResponse.getBytes("UTF-8").length);
+
+                OutputStream os = exchange.getResponseBody();
+                os.write(jsonResponse.getBytes("UTF-8"));
+                os.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                String errorMsg = "{\"error\": \"Échec de l'enregistrement de la réservation via RMI\"}";
+                exchange.sendResponseHeaders(500, errorMsg.length());
+                exchange.getResponseBody().write(errorMsg.getBytes());
+                exchange.getResponseBody().close();
+            }
+        } else {
+            exchange.sendResponseHeaders(405, -1);
+        }
     }
 
-    private void read(InputStream is) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'read'");
+    /**
+     * extraire une valeur d'une chaîne JSON simple
+     * sans utiliser de librairie externe.
+     */
+    private String extractValeur(String json, String cle) {
+        String recherche = "\"" + cle + "\":";
+        int debut = json.indexOf(recherche);
+        if (debut == -1) return "0"; // Évite les crashs si la clé n'existe pas
+
+        debut += recherche.length();
+        int fin = json.indexOf(",", debut);
+        if (fin == -1) fin = json.indexOf("}", debut);
+
+        // Nettoie les guillemets et espaces restants
+        return json.substring(debut, fin).replaceAll("[\"\\s}]", "").trim();
     }
 }
--- proxy/src/main/java/handlers/RestaurantsAppel.java
+++ proxy/src/main/java/handlers/RestaurantsAppel.java
@@ -2,24 +2,47 @@ package handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class RestaurantsAppel implements HttpHandler {
-    public void handle(HttpExchange t) throws IOException {
-        InputStream is = t.getRequestBody();
-        read(is); // .. read the request body
-        String response = "This is the response";
-        t.sendResponseHeaders(200, response.length());
-        OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
-    }
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        // Gestion du CORS
+        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
 
-    private void read(InputStream is) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'read'");
+        // Requete Preflight OPTIONS
+        if ("OPTIONS".equalsIgnoreCase(exchange.getRequestMethod())) {
+            exchange.sendResponseHeaders(204, -1);
+            return;
+        }
+
+        if ("GET".equalsIgnoreCase(exchange.getRequestMethod())) {
+            try {
+                Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+                ServiceRMI service = (ServiceRMI) registry.lookup("ServiceRestaurants");
+                String jsonResponse = service.getCoordonnees();
+
+                // Envoi de la réponse HTTP 200 avec le JSON
+                exchange.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
+                exchange.sendResponseHeaders(200, jsonResponse.getBytes("UTF-8").length);
+
+                OutputStream os = exchange.getResponseBody();
+                os.write(jsonResponse.getBytes("UTF-8"));
+                os.close();
+
+            } catch (Exception e) {
+                String errorMsg = "{\"error\": \"Erreur lors de la communication avec le serveur RMI\"}";
+                exchange.sendResponseHeaders(500, errorMsg.length());
+                exchange.getResponseBody().write(errorMsg.getBytes());
+                exchange.getResponseBody().close();
+            }
+        } else {
+            exchange.sendResponseHeaders(405, -1);
+        }
     }
 }
