e4d42f6a26ef43d049cd030560e107b4b9e7e8ef
Yanis Husser <yanis.husser2@etu.univ-lorraine.fr>
2026-06-14 17:47:22 +0200

    fix url du proxy

--- front/ts/config.ts
+++ front/ts/config.ts
@@ -1,4 +1,4 @@
-export const PROXY_URL = "http://100.64.80.232:8080";
+export const PROXY_URL = "http://100.64.80.232:8080/api";
 
 export const URL_VELIB_INFO = "https://api.cyclocity.fr/contracts/nancy/gbfs/v2/station_information.json";
 export const URL_VELIB_STATUS = "https://api.cyclocity.fr/contracts/nancy/gbfs/v2/station_status.json";
